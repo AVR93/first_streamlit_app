dad6bbed6251c70960d3eb62f8e34745c8b0967e
AVR93 <136744391+AVR93@users.noreply.github.com>
2023-06-23 15:48:14 -0500

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+import URLError
 
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Favorites')
