1cebb96cea009f627d3abd283bd737b21b9561b3
AVR93 <136744391+AVR93@users.noreply.github.com>
2023-06-23 16:02:46 -0500

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -35,8 +35,6 @@ fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
-streamlit.error()
-
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
